47701ab63958d4dd47aeea18898fd60e8b6489ca
Sergio Navarro <sirdio24@hotmail.com>
2019-11-14 21:03:41 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Requieres
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+// Inicializar Variables
+var app = express();
+
+
+
+// Conexion base de datos
+mongoose.connection.openUri('mongodb://localhost:/hospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'online');
+});
+
+
+
+
+
+//rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Expresr Server ports 3000: \x1b[32m%s\x1b[0m', 'online');
+});
